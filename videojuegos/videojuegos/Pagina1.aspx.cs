8de530f49049c7015f87e376a595691c734b1fb9
dai <dai@itam.mx>
2019-10-17 12:59:50 -0500

    Página 3

---
+++ videojuegos/videojuegos/Pagina1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Odbc;
+
+namespace videojuegos
+{
+    public partial class Pagina1 : System.Web.UI.Page
+    {
+        public OdbcConnection conectarBD()
+        {
+            String stringConexion = "Driver={SQL Server Native Client 11.0};Server=CC102-09\\SA;Uid=sa;Pwd=adminadmin;Database=gameSpot";
+            try
+            {
+                OdbcConnection conexion = new OdbcConnection(stringConexion);
+                conexion.Open();
+                lbContador.Text = "conexion exitosa";
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                lbContador.Text = ex.StackTrace.ToString();
+                return null;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btPagina2_Click(object sender, EventArgs e)
+        {
+            OdbcConnection miConexion = conectarBD();
+            
+            if(miConexion != null){
+                String query = "SELECT claveU FROM usuario WHERE email='" + txUsuario.Text + "'AND password='" + txContraseña.Text + "'";
+                OdbcCommand cmd = new OdbcCommand(query, miConexion);
+                OdbcDataReader rd = cmd.ExecuteReader();
+                if (rd.HasRows){
+                    rd.Read();
+                    Session["claveUnica"] = rd.GetInt32(0).ToString();
+                    Response.Redirect("./Pagina2.aspx");
+                }
+                else{
+                    lbContador.Text = "El usuario o contraseña son incorrectos";
+                }
+            }
+        }
+    }
+}
---
+++ videojuegos/videojuegos/Pagina3.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace videojuegos
+{
+    public partial class Pagina3 : System.Web.UI.Page
+    {
+        public OdbcConnection conectarBD()
+        {
+            String stringConexion = "Driver={SQL Server Native Client 11.0};Server=CC102-09\\SA;Uid=sa;Pwd=adminadmin;Database=gameSpot";
+            try
+            {
+                OdbcConnection conexion = new OdbcConnection(stringConexion);
+                conexion.Open();
+                lbError.Text = "conexion exitosa";
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                lbError.Text = ex.StackTrace.ToString();
+                return null;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OdbcConnection miConexion = conectarBD();
+            if (miConexion != null)
+            {
+                String query = String.Format("SELECT juegos.nombre from juegos, escriben WHERE escriben.claveU={0} AND escriben.claveJ=juegos.claveJ",Session["claveUnica"].ToString());
+                OdbcCommand cmd = new OdbcCommand(query, miConexion);
+                OdbcDataReader rd = cmd.ExecuteReader();
+                ddJuegos.Items.Clear();
+                while (rd.Read())
+                {
+                    ddJuegos.Items.Add(rd.GetString(0));
+                }
+                rd.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("./Pagina4.aspx");
+        }
+
+        protected void ddJuegos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OdbcConnection miConexion = conectarBD();
+            if (miConexion != null)
+            {
+                String query = String.Format("SELECT claveJ FROM juegos WHERE nombre='{0}'",ddJuegos.SelectedValue);
+                OdbcCommand cmd = new OdbcCommand(query, miConexion);
+                OdbcDataReader rd = cmd.ExecuteReader();
+                rd.Read();
+                int claveJuego = rd.GetInt32(0);
+
+                String query2 = String.Format("SELECT critica.contenido FROM critica WHERE critica.claveC= (SELECT claveC FROM escriben WHERE escriben.claveU={0} and escriben.claveJ={1})",  Session["claveUnica"].ToString(), claveJuego);
+                OdbcCommand cmd2 = new OdbcCommand(query2, miConexion);
+                OdbcDataReader rd2 = cmd.ExecuteReader();
+                rd2.Read();
+                lbCritica.Text= rd.GetString(0);
+                rd.Close();
+                rd2.Close();
+            }
+            else
+                Console.WriteLine("Hola");
+        }
+    }
+}
